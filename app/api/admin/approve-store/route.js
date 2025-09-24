f9c1d133650b2e813ebb03281c9b1912ec4c1906
Kapil <kapilraut026@gmail.com>
2025-09-24 22:23:44 +0545

    authorized...

--- app/api/admin/approve-store/route.js
+++ app/api/admin/approve-store/route.js
@@ -1,4 +1,7 @@
-
+import prisma from "@/lib/prisma";
+import authAdmin from "@/middlewares/authAdmin"
+import {getAuth} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // approve seller
 
